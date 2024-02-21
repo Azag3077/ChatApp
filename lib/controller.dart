1d526bfa82e7aa46e42bd4fcd8b8b99c448aa83c
Azag.py <agboolaodunayo2016@gmail.com>
2024-02-21 21:33:06 +0100

    Rough update

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,22 +1,44 @@
 import 'package:chatapp/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'pages/chat_lists.dart';
 import 'pages/conversation.dart';
+import 'pages/create_account.dart';
 
 mixin Controller on Widget {
   Future<void> initialize(BuildContext context) async {
-    FirebaseService.checkUserExistence();
-    Future.delayed(const Duration(seconds: 2)).then(
-      (value) => Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const ChatList())),
-    );
+    SharedPreferences.getInstance().then((prefs) {
+      final uid = prefs.get('uid');
+
+      if (uid == null) {
+        _gotoPage(context, const CreateAccount());
+      } else {
+        _gotoPage(context, const ConversationPage());
+      }
+    });
+
+// // Save an integer value to 'counter' key.
+//     await prefs.setInt('counter', 10);
+// // Save an boolean value to 'repeat' key.
+//     await prefs.setBool('repeat', true);
+// // Save an double value to 'decimal' key.
+//     await prefs.setDouble('decimal', 1.5);
+// // Save an String value to 'action' key.
+//     await prefs.setString('action', 'Start');
+// // Save an list of strings to 'items' key.
+//     await prefs.setStringList('items', <String>['Earth', 'Moon', 'Sun']);
+
+    // FirebaseService.checkUserExistence();
+    // Future.delayed(const Duration(seconds: 2)).then(
+    //   (value) => Navigator.pushReplacement(
+    //       context, MaterialPageRoute(builder: (context) => const ChatList())),
+    // );
   }
 
   void onChatPressed(BuildContext context, WidgetRef ref) {
     FirebaseService.checkUserExistence();
-    // _gotoPage(context, const ConversationPage());
   }
 
   void _gotoPage(BuildContext context, Widget page) {
---
+++ lib/pages/create_account.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatefulWidget {
+  const CreateAccount({super.key});
+
+  @override
+  State<CreateAccount> createState() => _CreateAccountState();
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: AppBar(
+        //   title: Text('Xag'),
+        // ),
+        // body: Column(
+        //   children: [
+        //     CircleAvatar(
+        //       backgroundImage: AssetImage('assets/avater/img_12.png'),
+        //     ),
+        //     Text('Create Account'),
+        //   ],
+        // ),
+        );
+  }
+}
